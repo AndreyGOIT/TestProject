2b6d3acb09192dc0c04c727b589d5ec99bb94dc7
AndreyGOIT <ae.borga@gmail.com>
2024-09-10 12:44:45 +0300

    add AND operator

--- Program.cs
+++ Program.cs
@@ -15,7 +15,13 @@ if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)) {
     total += 2;
 }
 
-if (total  > 14) {
+if ((roll1 == roll2) && (roll2 == roll3)) 
+{
+    Console.WriteLine("You rolled triples! +6 bonus to total!");
+    total += 6;
+}
+
+if (total  >= 15) {
     Console.WriteLine("You win!");
 }
 
@@ -23,10 +29,10 @@ if (total < 15) {
     Console.WriteLine("Sorry, you lose.");
 }
 
-string message = "The quick brown fox jumps over the lazy dog.";
-bool result = message.Contains("dog");
-Console.WriteLine(result);
+// string message = "The quick brown fox jumps over the lazy dog.";
+// bool result = message.Contains("dog");
+// Console.WriteLine(result);
 
-if (message.Contains("fox")) {
-    Console.WriteLine("What does the fox say?");
-}
+// if (message.Contains("fox")) {
+//     Console.WriteLine("What does the fox say?");
+// }
