4382c96ac1a424a0e64958701635626504d2a750
AndreyGOIT <ae.borga@gmail.com>
2024-09-10 13:03:08 +0300

    game with prize for winer

--- Program.cs
+++ Program.cs
@@ -5,6 +5,9 @@ Random dice = new Random();
 int roll1 = dice.Next(1, 7);
 int roll2 = dice.Next(1, 7);
 int roll3 = dice.Next(1, 7);
+// roll1 = 6;
+// roll2 = 6;
+// roll3 = 6;
 
 int total = roll1 + roll2 + roll3;
 
@@ -22,14 +25,25 @@ if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
         Console.WriteLine("You rolled doubles! +2 bonus to total!");
         total += 2;
     }
+
+    Console.WriteLine($"Your total including the bonus: {total}");
 }
 
-if (total  >= 15) {
-    Console.WriteLine("You win!");
-}
-else 
+if (total >= 16)
 {
-    Console.WriteLine("Sorry, you lose.");
+    Console.WriteLine("You win a new car!");
+}
+else if (total >= 10)
+{
+    Console.WriteLine("You win a new laptop!");
+}
+else if (total == 7)
+{
+    Console.WriteLine("You win a trip for two!");
+}
+else
+{
+    Console.WriteLine("You win a kitten!");
 }
 
 // string message = "The quick brown fox jumps over the lazy dog.";
--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f7ce0ee3d8b92c4ad562d781339e237e1fff3d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22eb90e6ea4c5c11a69fadef1f6475d29b76e498")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
