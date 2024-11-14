4848200fb31029137125a024b3e72d270d58fe2f
AndreyGOIT <ae.borga@gmail.com>
2024-11-14 16:31:36 +0200

    the FizzBuzz challenge

--- Documents/CsharpProjects/Program.cs
+++ Documents/CsharpProjects/Program.cs
@@ -1,8 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
 
-string[] names = { "Alex", "Eddie", "David", "Michael" };
-for (int i = names.Length - 1; i >= 0; i--)
+// string[] names = { "Alex", "Eddie", "David", "Michael" };
+// for (int i = names.Length - 1; i >= 0; i--)
+// {
+//     Console.WriteLine(names[i]);
+// }
+
+for (int i = 1; i <= 100; i++)
 {
-    Console.WriteLine(names[i]);
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine(i  + " - FizzBuzz");
+    }
+    else if (i % 3 == 0) {
+        Console.WriteLine(i + " - Fizz");
+    }
+    else if (i % 5 == 0) {
+        Console.WriteLine(i + " - Buzz");
+    }
+    else {
+        Console.WriteLine(i);
+    }
 }
