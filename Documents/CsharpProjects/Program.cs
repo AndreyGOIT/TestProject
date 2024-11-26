0317bf186f784e8dd0f38cf7049089bed2aa8802
AndreyGOIT <ae.borga@gmail.com>
2024-11-26 15:02:10 +0200

    do-while Code project 3 fixed

--- Documents/CsharpProjects/Program.cs
+++ Documents/CsharpProjects/Program.cs
@@ -38,68 +38,68 @@
 #endregion
 
 #region Code project 2 - write code that validates string input
-string? readResult;
-string roleName = "";
-bool validEntry = false;
+// string? readResult;
+// string roleName = "";
+// bool validEntry = false;
 
-do
-{                
-    Console.WriteLine("Enter your role name (Administrator, Manager, or User)");
-    readResult = Console.ReadLine();
-    if (readResult != null) 
-    {
-        roleName = readResult.Trim();
-    }
+// do
+// {                
+//     Console.WriteLine("Enter your role name (Administrator, Manager, or User)");
+//     readResult = Console.ReadLine();
+//     if (readResult != null) 
+//     {
+//         roleName = readResult.Trim();
+//     }
 
-    if (roleName.ToLower() == "administrator" || roleName.ToLower() == "manager" || roleName.ToLower() == "user") 
-    {
-        validEntry = true;
-    }
-    else
-    {
-        Console.Write($"The role name that you entered, \"{roleName}\" is not valid. ");
-    }
+//     if (roleName.ToLower() == "administrator" || roleName.ToLower() == "manager" || roleName.ToLower() == "user") 
+//     {
+//         validEntry = true;
+//     }
+//     else
+//     {
+//         Console.Write($"The role name that you entered, \"{roleName}\" is not valid. ");
+//     }
 
-} while (validEntry == false);
+// } while (validEntry == false);
 
-Console.WriteLine($"Your input value ({roleName}) has been accepted.");
-readResult = Console.ReadLine();
+// Console.WriteLine($"Your input value ({roleName}) has been accepted.");
+// readResult = Console.ReadLine();
 #endregion
 
 #region Code project 3 - write code that validates string input
-// string[] myStrings = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" };
-// int stringsCount = myStrings.Length;
-// string myString = "";
-// int periodLocation = 0;
-// for (int i = 0; i < stringsCount; i++)
-// {
-//     myString = myStrings[i];
-//     periodLocation = myString.IndexOf(".");
-//     string mySentence = "";
-//     // extract sentences
-//     if (periodLocation == -1)
-//     {
-//         mySentence = myString;
-//     }
-//     else
-//     {
-//         mySentence = myString.Substring(0, periodLocation);
-//     }
-//     // print sentences
-//     Console.WriteLine("first sentence: " + mySentence);
-//     //     mySentence + " " + mySentence + " " + mySentence
-//     // );
-//     // get the index of the next sentence
-//     myString = myString.Substring(periodLocation + 1);
-//     periodLocation = myString.IndexOf(".");
-//     if (periodLocation == -1)
-//     {
-//         mySentence = myString;
-//     }
-//     else
-//     {
-//         mySentence = myString.Substring(0, periodLocation);
-//     }
-//     Console.WriteLine("Last sentence: " + mySentence);
-// }
+string[] myStrings = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" };
+int stringsCount = myStrings.Length;
+
+string myString = "";
+int periodLocation = 0;
+
+for (int i = 0; i < stringsCount; i++)
+{
+    myString = myStrings[i];
+    periodLocation = myString.IndexOf(".");
+
+    string mySentence;
+
+    // extract sentences from each string and display them one at a time
+    while (periodLocation != -1)
+    {
+
+        // first sentence is the string value to the left of the period location
+        mySentence = myString.Remove(periodLocation);
+
+        // the remainder of myString is the string value to the right of the location
+        myString = myString.Substring(periodLocation + 1);
+
+        // remove any leading white-space from myString
+        myString = myString.TrimStart();
+
+        // update the comma location and increment the counter
+        periodLocation = myString.IndexOf(".");
+
+        Console.WriteLine(mySentence);
+    }
+ 
+    mySentence = myString.Trim();
+    Console.WriteLine(mySentence);
+}
 #endregion
