a3a428c5a60689e01eecf230cb88e0645e84801d
AndreyGOIT <ae.borga@gmail.com>
2024-12-10 12:53:07 +0200

    add new project

--- Documents/CsharpProjects/obj/Debug/net8.0/CsharpProjects.AssemblyInfo.cs
+++ Documents/CsharpProjects/obj/Debug/net8.0/CsharpProjects.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CsharpProjects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d583d57b28a422b43db54b574a2ad4b890759473")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0317bf186f784e8dd0f38cf7049089bed2aa8802")]
 [assembly: System.Reflection.AssemblyProductAttribute("CsharpProjects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CsharpProjects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
